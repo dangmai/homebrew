6f046cbddfc0b89f010954b085ee5ea02eb285f3
Dang Mai <contact@dangmai.net>
2013-05-29 12:59:05 -0400

    Make sure installation is run using run_as

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,10 +3,12 @@ homebrew_go = "#{Chef::Config[:file_cache_path]}/homebrew_go"
 remote_file homebrew_go do
   source "https://raw.github.com/mxcl/homebrew/go"
   mode 00755
+  owner node['homebrew']['run_as']
 end
 
 execute homebrew_go do
   not_if { File.exist? '/usr/local/bin/brew' }
+  user node['homebrew']['run_as']
 end
 
 package 'git' do
@@ -15,4 +17,5 @@ end
 
 execute "update homebrew from github" do
   command "/usr/local/bin/brew update || true"
+  user node['homebrew']['run_as']
 end
